e6786c2ecf8828d91b2ff23f9f8c2e80eb5a8a92
Mark Harding <mark@kramnorth.com>
2015-07-16 15:18:34 +0100

    styling fixes

--- www/js/services/calls/CallReceiver.js
+++ www/js/services/calls/CallReceiver.js
@@ -8,66 +8,66 @@
 define(['angular', 'socketio'], function(angular) {
     "use strict";
 
-	var factory = function($rootScope, $timeout, $interval, $ionicModal, socket, push, Client) {
+    var factory = function($rootScope, $timeout, $interval, $ionicModal, socket, push, Client) {
 
-		//the modal will inherit this scope
-		var $scope = $rootScope.$new();
+        //the modal will inherit this scope
+        var $scope = $rootScope.$new();
 
-		socket.on('messageReceived', function(guid, message) {
-			switch (message.type){
-				case 'call':
-					if (!$rootScope.inCall) {
-						incomingCall(guid, {name: "..."});
-						socket.emit('sendMessage', guid, {type:'available'});
-					} else {
+        socket.on('messageReceived', function(guid, message) {
+            switch (message.type){
+                case 'call':
+                    if (!$rootScope.inCall) {
+                        incomingCall(guid, {name: "..."});
+                        socket.emit('sendMessage', guid, {type:'available'});
+                    } else {
             console.log('engaged..');
-					//	socket.emit('sendMessage', guid, {type:'engaged'});
-					}
-					break;
+                    //  socket.emit('sendMessage', guid, {type:'engaged'});
+                    }
+                    break;
 
-			}
-		});
+            }
+        });
 
-		push.listen('call', function(data) {
+        push.listen('call', function(data) {
       console.log(data, data.json);
       incomingCall(data.json.from_guid, {name: data.json.from_name});
-		//	console.log(data);
-		});
+        //  console.log(data);
+        });
 
-		var incomingCall = function(guid, user, offer) {
-			if ($rootScope.inCall)
-				return false;
+        var incomingCall = function(guid, user, offer) {
+            if ($rootScope.inCall)
+                return false;
 
-			$scope.callConfig = {
-				initiator: false,
-				guid: guid,
-				name: user.name
-			};
+            $scope.callConfig = {
+                initiator: false,
+                guid: guid,
+                name: user.name
+            };
 
-			//get the profile of who is calling
-			Client.get('api/v1/channel/' + guid, {},
-				function(success) {
-					$scope.callConfig.name = success.channel.name;
-				},
-				function(error) {
-				});
+            //get the profile of who is calling
+            Client.get('api/v1/channel/' + guid, {},
+                function(success) {
+                    $scope.callConfig.name = success.channel.name;
+                },
+                function(error) {
+                });
 
-			$timeout(function() {
-				$ionicModal.fromTemplateUrl('templates/gatherings/chat/call.html', {
-					scope: $scope,
-					animation: 'slide-in-up',
+            $timeout(function() {
+                $ionicModal.fromTemplateUrl('templates/gatherings/chat/call.html', {
+                    scope: $scope,
+                    animation: 'slide-in-up',
           backdropClickToClose: false,
-					hardwareBackButtonClose: false
-				}).then(function(modal) {
-					$scope.modal = modal;
-					$scope.modal.show();
+                    hardwareBackButtonClose: false
+                }).then(function(modal) {
+                    $scope.modal = modal;
+                    $scope.modal.show();
           document.getElementById('ringing').play();
           $rootScope.inCall = true;
-				});
-			});
-		};
+                });
+            });
+        };
 
-		return {};
+        return {};
 
     };
 
