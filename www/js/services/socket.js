d7c6bfd5f8fcce392df505a58ebbb6ff49a44ea1
Mark Harding <mark@kramnorth.com>
2015-09-10 10:31:41 +0100

    socket reconnect fixes

--- www/js/services/socket.js
+++ www/js/services/socket.js
@@ -8,24 +8,29 @@
 define(['angular', 'socketio'], function(angular, io) {
     "use strict";
 
-    var factory = function($rootScope, storage) {
+    var factory = function($rootScope, storage, $interval) {
 
-		var socket = io.connect('https://ha-socket-io-us-east-1.minds.com:3030', {
-						'reconnect': true
+  		var socket = io.connect('https://ha-socket-io-us-east-1.minds.com:3030', {
+						'reconnect': true,
+             'reconnection': true,
+             'timeout': 40000
 						});
-		socket.on('connect', function() {
-			socket.emit('register', $rootScope.user_guid, storage.get('access_token'));
-		});
+  		socket.on('connect', function() {
+  			socket.emit('register', $rootScope.user_guid, storage.get('access_token'));
+  		});
 
-    socket.on('disconnect', function() {
-      socket.io.reconnect();
-      console.log('trying to reconnect...');
-    });
+      socket.on('disconnect', function() {
+        var reconnect = $interval(function(){
+          if(socket.connected)
+            return $interval.cancel(reconnect);
+          socket.io.connect();
+        },1000);
+      });
 
-		return socket;
+  		return socket;
 
     };
 
-    factory.$inject = ['$rootScope', 'storage'];
+    factory.$inject = ['$rootScope', 'storage', '$interval'];
     return factory;
 });
